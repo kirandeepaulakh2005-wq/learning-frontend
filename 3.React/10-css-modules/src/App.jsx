00583a130e7696173d35c710f81057bbb8fec773
kirandeepaulakh2005-wq <kirandeepaulakh2005@gmail.com>
2026-08-12 11:04:24 +0530

    container

--- 3.React/10-css-modules/src/App.jsx
+++ 3.React/10-css-modules/src/App.jsx
@@ -1,13 +1,25 @@
-import './App.css'
+import style from './App.module.css';
+import Container from './Container';
 function App() {
 
 
   return (
     <>
-    <div className="heading">hello</div>
-      
+    <Container>
+    <h1 className={style.heading}>hello</h1>
+    <p className={`${style.para} ${style.heading}`}>
+      This is my paragraph.
+    </p>
+    </Container>
+   <Container>
+    <p>This is random text.</p>
+   </Container>
+    <h1 className={style.heading}>hello</h1>
+    <p className={`${style.para} ${style.heading}`}>
+      This is my paragraph.
+    </p>
     </>
   )
 }
 
-export default App
+export default App;
