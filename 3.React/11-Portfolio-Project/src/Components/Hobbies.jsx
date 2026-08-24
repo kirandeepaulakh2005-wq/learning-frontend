6533d67fc9d59feae1f9021e0880909136ac693f
kirandeepaulakh2005-wq <kirandeepaulakh2005@gmail.com>
2026-08-25 00:04:48 +0530

    hobbies

--- 3.React/11-Portfolio-Project/src/Components/Hobbies.jsx
+++ 3.React/11-Portfolio-Project/src/Components/Hobbies.jsx
@@ -4,7 +4,6 @@ const Hobbies =() =>{
     return <Section icon={<Award/>}sectionTitle="Hobbies & Interests">
  <p>
 I'm Kirandeep Kaur, a B.Tech Computer Science Engineering student with a strong interest in web development. I enjoy creating clean, responsive, and user-friendly websites using HTML, CSS, JavaScript, React, and Tailwind CSS.
-I'm continuously improving my coding and problem-solving skills by working on projects and learning new technologies.
 </p>
 </Section>;
 }
